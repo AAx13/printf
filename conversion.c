0a728b72f3419609d7c2a622cd228872c240a881
DamianAli <damian.ali33@gmail.com>
2017-03-01 21:13:10 -0800

    Task 1- running against hbtn checks.

--- conversion.c
+++ conversion.c
@@ -21,7 +21,7 @@ int conv(va_list args, const char *format, int len, int count, int count2)
 	{
 		case 'c':
 			ui = va_arg(args, int);
-			if (ui == -1)
+			if (ui == -1 || ui == 0)
 			{
 				count2 += print_str("(null)");
 				break;
