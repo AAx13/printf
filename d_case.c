de21e455a38a7fd27956262c6dadb60b5cf485d7
DamianAli <damian.ali33@gmail.com>
2017-03-03 21:44:10 -0800

    i,d_case - allowed for INT_MAX and INT_MIN as an integer input.

--- d_case.c
+++ d_case.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * d_case - handle integer conversion specifiers.
--- print_int.c
+++ print_int.c
@@ -9,24 +9,27 @@
  */
 int print_int(int n)
 {
-	int len, ncpy, bytecnt;
+	int len, ncpy, bytecnt, digit;
 
 	bytecnt = 0;
 	if (n < 0)
 	{
-		n *= -1;
 		bytecnt += _putchar('-');
 	}
+	else
+	{
+		n = -n;
+	}
 
-	for (len = 1, ncpy = n; ncpy > 0; len *= 10)
+	for (len = 1, ncpy = n; ncpy < -9; len *= 10)
 	{
 		ncpy /= 10;
 	}
 
-	len /= 10;
-	for (ncpy = n; ncpy > 0; len /= 10)
+	for (ncpy = n; len != 0; len /= 10)
 	{
-		bytecnt += _putchar((ncpy / len) + '0');
+		digit = (ncpy / len) * -1;
+		bytecnt += _putchar(digit + '0');
 		ncpy %= len;
 	}
 
